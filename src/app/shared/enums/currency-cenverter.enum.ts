547cbf79481584b04d3e9f2fcc1756da8d57b1f6
Hossam Zakraia <hzakaria@youxel.com>
2022-10-01 07:04:15 +0200

    [FF2] [Portal] [feature] add currency converter UI

---
+++ src/app/shared/enums/currency-cenverter.enum.ts
@@ -0,0 +1,7 @@
+export enum ECurrencyConverter {
+  MORE_DETAILS = 'More Details',
+  CONVERT = 'Convert',
+  SWAP = ' swap ',
+  FROM = 'From',
+  TO = 'To',
+}
--- src/app/shared/modules/currency-converter/currency-converter.component.ts
+++ src/app/shared/modules/currency-converter/currency-converter.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ECurrencyConverter } from '../../enums/currency-cenverter.enum';
 
 @Component({
   selector: 'app-currency-converter',
   templateUrl: './currency-converter.component.html',
-  styleUrls: ['./currency-converter.component.scss']
+  styleUrls: ['./currency-converter.component.scss'],
 })
 export class CurrencyConverterComponent implements OnInit {
+  // currency converter enum
+  EcurrencyConverter = ECurrencyConverter;
+  // currencys Names
+  currencys: any = ['Florida', 'South Dakota', 'Tennessee', 'Michigan'];
+  constructor(public fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  /***** form *****/
+  converterForm = this.fb.group({
+    from: ['Florida'],
+    to: ['South Dakota'],
+  });
 
+  /**
+   * `changeCurrency()`
+   * @description get selected value
+   * @param  event event of selected
+   */
+  changeCurrency = (event: any) => {
+    console.log(event.value);
+    // this.converterForm.controls['to'].disable();
+  };
+
+  /**
+   * `swap()`
+   * @description to swap value between from and to
+   */
+  swap = () => {
+    const toValue = this.converterForm.controls['to'].value;
+    const fromValue = this.converterForm.controls['from'].value;
+
+    this.converterForm.controls['to'].setValue(fromValue);
+    this.converterForm.controls['from'].setValue(toValue);
+  };
+
+  /**
+   * `submit()`
+   * @description button to convert currency value
+   */
+  submit = () => {
+    if (!this.converterForm.valid) {
+      console.log('ss');
+    } else {
+      console.log(JSON.stringify(this.converterForm.value));
+    }
+  };
 }
